e6569495c1911b10a2c9c6107e3d5ff6d3d621ad
Ana Gonzalez <anagonm@gmail.com>
2018-03-25 13:31:48 +0200

    [Added] new characters in Data file

--- src/data/Data.js
+++ src/data/Data.js
@@ -8,7 +8,6 @@ var Data = {
     {id: 5, name: "Stark"}
   ],
 
-
   characters: [
 
     {
@@ -22,29 +21,37 @@ var Data = {
       description: "Daenerys Targaryen, es un personaje de ficción de la saga de literatura fantástica Canción de hielo y fuego, escrita por George R. R. Martin. Daenerys es uno de los personajes principales y posee capítulos narrados desde su punto de vista en Juego de tronos, Choque de reyes, Tormenta de espadas y Danza de dragones. En la adaptación televisiva Juego de tronos, el personaje es interpretado por la actriz Emilia Clarke. <br /><br />Daenerys fue la única hija del rey Aerys II Targaryen y de su hermana-esposa, la reina Rhaella. Tras la muerte de su padre y de su hermano mayor en la Guerra del Usurpador, su madre murió dando a luz a la pequeña Daenerys. De esa manera, su hermano mayor Viserys quedó como legítimo rey Targaryen al Trono de Hierro y ella como su heredera."
     },
     {
-      id: 2,
+      id: 4,
       stars: 4,
-      avatar: "daenarys.png",
-      name: "Juan Ty",
-      alias: "Dany, Daenerys de la tormenta, Khaleesi",
-      nacimiento: "284 AC",
-      familia: "Casa Targayren",
-      description: "Daenerys Targaryen, es un personaje de ficción de la saga de literatura fantástica Canción de hielo y fuego, escrita por George R. R. Martin. Daenerys es uno de los personajes principales y posee capítulos narrados desde su punto de vista en Juego de tronos, Choque de reyes, Tormenta de espadas y Danza de dragones. En la adaptación televisiva Juego de tronos, el personaje es interpretado por la actriz Emilia Clarke. <br /><br />Daenerys fue la única hija del rey Aerys II Targaryen y de su hermana-esposa, la reina Rhaella. Tras la muerte de su padre y de su hermano mayor en la Guerra del Usurpador, su madre murió dando a luz a la pequeña Daenerys. De esa manera, su hermano mayor Viserys quedó como legítimo rey Targaryen al Trono de Hierro y ella como su heredera."
+      avatar: "jon.png",
+      name: "Jon Snow",
+      alias: "Lord Nieve, El Bastardo de Invernalia",
+      nacimiento: "283 AC",
+      familia: "Casa Stark",
+      description: "Lord Jon Nieve es el Lord Comandante de la Guardia de la Noche nº 998. Es hijo bastardo de Lord Eddard Stark. Su lobo huargo, Fantasma, es albino y mudo. El emblema personal de Jon consiste en los colores de la Casa Stark invertidos, un lobo huargo de plata corriendo sobre fondo de cenizo. Jon creció como un norteño más, y adquirió de varias maneras los valores del honor, a los que intenta apelar incluso cuando se enfrenta a decisiones complejas y moralmente ambiguas. Idolatra a Lord Eddard, pero se siente frustrado por su negativa a nombrar o hablar de su madre. Tiene un punto de vista perceptivo y observador, mostrando valentía, ingenio y agilidad de pensamiento durante su tiempo en el Muro, llegando a ser un líder natural y en la Guardia de la Noche."
     },
     {
       id: 3,
       stars: 4,
-      avatar: "daenarys.png",
-      name: "Pepe Targayren",
-      alias: "Dany, Daenerys de la tormenta, Khaleesi",
-      nacimiento: "284 AC",
-      familia: "Casa Targayren",
-      description: "Daenerys Targaryen, es un personaje de ficción de la saga de literatura fantástica Canción de hielo y fuego, escrita por George R. R. Martin. Daenerys es uno de los personajes principales y posee capítulos narrados desde su punto de vista en Juego de tronos, Choque de reyes, Tormenta de espadas y Danza de dragones. En la adaptación televisiva Juego de tronos, el personaje es interpretado por la actriz Emilia Clarke. <br /><br />Daenerys fue la única hija del rey Aerys II Targaryen y de su hermana-esposa, la reina Rhaella. Tras la muerte de su padre y de su hermano mayor en la Guerra del Usurpador, su madre murió dando a luz a la pequeña Daenerys. De esa manera, su hermano mayor Viserys quedó como legítimo rey Targaryen al Trono de Hierro y ella como su heredera."
+      avatar: "arya.png",
+      name: "Arya Stark",
+      alias: "Cambiapieles, Entrelospiés, Arry",
+      nacimiento: "289 AC",
+      familia: "Casa Stark",
+      description: "Arya es una niña voluntariosa y con interés en las actividades típicamente masculinas como el combate y la exploración. No tiene una buena relación con su hermana Sansa, cuyos intereses son tradicionalmente femeninos, actividades en las que Arya no tiene habilidades. Es animada en sus gustos por su medio-hermano Jon, con quien tiene una relación cercana. A través de sus viajes, Arya demuestra gran ingenio, astucia y la capacidad firme de aceptar las necesidades, por duras que sean. Se dice que su temperamento es similar al de su tía Lyanna"
+    },
+    {
+      id: 2,
+      stars: 2,
+      avatar: "tyrion.png",
+      name: "Tyrion Lannister",
+      alias: "El Gnomo, Mediohombre",
+      nacimiento: "273 AC",
+      familia: "Casa Lannister",
+      description: "Tyrion fue el tercer hijo de Lord Tywin Lannister y Lady Joanna Lannister, siendo hermano menor de los gemelos Jaime y Cersei, quienes ya tenían nueve años. Su madre murió durante el parto y como resultado, su padre le culpa a él de la muerte de su esposa y le odia por ser deforme. Tyrion se encontró marginado de la vida regular de una familia, especialmente por parte de su padre y hermana, quienes abusaron psicológicamente de él durante su infancia. Tyrion tiene piernas cortas y torcidas, una frente prominente, ojos desiguales de color verde y negro, cabello lacio y rubio casi blanco. Más tarde se deja crecer la barba, que es de color negro y rubio.[1] Después de la Batalla del Aguasnegras, queda con una cicatriz espantosa en la cara y pierde la mayor parte de la nariz."
     }
 
   ]
-
-
 }
 
 
